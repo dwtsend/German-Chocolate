7d260a85cb77336e0bbc4ea5a2f48d66c4e94c73
jrtsend <jrtsend@gmail.com>
2018-09-02 21:47:09 -0400

    Adds camera controls and custom cursor

---
+++ GermanChocolate/Assets/Scripts/MouseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public Texture2D mainCursorTexture;
+    private Vector2 mainHotSpot;
+    public Texture2D selectionCursorTexture;
+    private Vector2 selectionHotSpot;
+    private CursorMode cursorMode = CursorMode.Auto;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainHotSpot = Vector2.zero;
+        selectionHotSpot = new Vector2 (16f, 16f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit rayHit;
+
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            Debug.Log(rayHit.transform.gameObject.tag);
+            switch (rayHit.transform.gameObject.tag)
+            {
+                case "Unit":
+                    Cursor.SetCursor(selectionCursorTexture, selectionHotSpot, cursorMode);
+                    break;
+                default:
+                    Cursor.SetCursor(mainCursorTexture, mainHotSpot, cursorMode);
+                    break;
+            }
+        }
+
+       
+	}
+
+}
